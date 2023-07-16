e742c5d69034a379b4d95a19354340c16afa94c5
Yuna <ksedih@rambler.ru>
2023-07-16 13:59:12 +0300

    Мастер-ветка ДЗ5

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+
+        int clientOS = 1;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+
+
+
+    }
+}
